a776d75fd8b2836c7c002b7cad2e18bd958d8a17
许继元 <490601115@qq.com>
2020-10-22 22:40:09 +0800

    feat: clear data

--- wearer_status_detect/data_processing/add_label.py
+++ wearer_status_detect/data_processing/add_label.py
@@ -36,6 +36,9 @@ if __name__ == '__main__':
     dataSet_lie = load_data("../data/data_generated_by_QG/lable1躺.txt", 1)
     dataSet_walk = load_data("../data/data_generated_by_QG/lable2走.txt", 2)
     dataSet_run = load_data("../data/data_generated_by_QG/lable3跑.txt", 3)
+
+    dataSet_sit = dataSet_sit[~dataSet_sit['feature_1'].isin([0.0])]
+
     dataSet_sit['label'] = dataSet_sit['label'].map(lambda x: int(x))
     dataSet_lie['label'] = dataSet_lie['label'].map(lambda x: int(x))
     dataSet_walk['label'] = dataSet_walk['label'].map(lambda x: int(x))
